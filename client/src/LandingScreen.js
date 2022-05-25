23c0b7b92ae9bd7aa77afd5cf2851b3e780e7f44
Joshua <tan.yin.feng@hotmail.com>
2022-05-25 16:56:42 +0800

    Beautify landing screen

--- client/src/LandingScreen.js
+++ client/src/LandingScreen.js
@@ -4,10 +4,10 @@ export default function LandingScreen() {
   const [, setMainScreenContext] = useContext(MainScreenContext);
 
   return (
-    <div className="grid place-content-center text-center h-full border-2 gap-y-2 py-10">
-      <div className="text-xl font-bold">Welcome!</div>
+    <div className="grid place-content-center text-center h-full border-2 border-opacity-50 shadow-md rounded-md border-gray-100 gap-y-2 py-10">
+      <div className="text-3xl font-bold text-purple-400">Welcome!</div>
       <div
-        className="rounded-md bg-purple-400 p-2 hover:bg-purple-500 hover:text-white cursor-pointer transition-colors ease-in-out"
+        className="rounded-md text-purple-400 p-2 hover:text-white hover:bg-purple-400 border-2 bg-white border-purple-400 cursor-pointer transition-colors ease-in-out"
         onClick={() => setMainScreenContext(null, "create")}
       >
         Create a new post
