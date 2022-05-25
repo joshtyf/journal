eea1e19cb2e4d929866dbe4ecf9eba6baab20389
Joshua <tan.yin.feng@hotmail.com>
2022-05-25 16:38:34 +0800

    Add ability to close all screens

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { createContext, useState } from "react";
 import Reader from "./Reader";
 import MyEditor from "./MyEditor";
 import LandingScreen from "./LandingScreen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export const MainScreenContext = createContext();
 
@@ -22,9 +24,17 @@ export default function App() {
     >
       <div className="p-4 flex space-x-2">
         <div className="w-1/6 border-r-2">
-          <Sidebar selectedPostId={selectedPostId}/>
+          <Sidebar selectedPostId={selectedPostId} />
         </div>
-        <div className="w-5/6">
+        <div className="flex flex-col w-5/6">
+          {mode !== null && (
+            <div
+              className="self-end text-red-400 hover:text-red-500 transition-colors ease-in-out cursor-pointer"
+              onClick={() => setMainScreenContext(null, null)}
+            >
+              <FontAwesomeIcon icon={faXmark} size="2x" />
+            </div>
+          )}
           {mode === "view" ? (
             <Reader postId={selectedPostId} />
           ) : mode === "edit" ? (
