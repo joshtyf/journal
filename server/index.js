db412c1b95e4a4ec301b2c97be98e0b2c7d99b1b
Joshua <tan.yin.feng@hotmail.com>
2022-05-27 17:47:06 +0800

    Test heroku

--- server/index.js
+++ server/index.js
@@ -30,13 +30,14 @@ if (env == "production") {
 }
 
 app.get("/api", (req, res) => {
-  getPosts()
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  res.send("okay");
+  // getPosts()
+  //   .then((response) => {
+  //     res.status(200).send(response);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).send(error);
+  //   });
 });
 
 app.get("/api/:id", (req, res) => {
